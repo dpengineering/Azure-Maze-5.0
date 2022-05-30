18fd9c5a3212999befce68920a0dd1792026c5ae
nikhi1g <89803008+nikhi1g@users.noreply.github.com>
2022-05-29 18:58:38 -0700

    Update maze_arduino.py

--- final/maze/maze_arduino.py
+++ final/maze/maze_arduino.py
@@ -1,16 +1,16 @@
-from final.imports.imports import *
+from final.imports.imports import * # import all useful from this path.
 
 
 class Ball_Pump:
     def __init__(self):
         self.board = Arduino('/dev/ttyUSB0')  # /dev/ttyUSB0 = port of the Arduino nano
-        with open("maze_arduino.txt", "r") as f:
+        with open("maze_arduino.txt", "r") as f: # before this class even runs, the coder must put the word 'right', or 'left' in maze_arduino.txt, if not already there.
             start_orientation = str(f.readline())
         self.orientation = start_orientation
 
 
 
-    # def log_new_position(self):
+    # def log_new_position(self): # old code that doesn't work
     #     with open("maze_arduino.txt", "r+") as f:
     #         if str(f.readline()) == "right":
     #             self.orientation = "left"
@@ -21,7 +21,7 @@ class Ball_Pump:
     #             f.truncate(0)
     #             f.write(str(self.orientation))
 
-    def pump(self):
+    def pump(self): # pumps the opposite pump of what was previously done
         if self.orientation == "left":
             self.pump_left_once()
             with open("maze_arduino.txt", "w") as f:
@@ -35,7 +35,7 @@ class Ball_Pump:
                 f.write("left")
             self.orientation = "left"
 
-    def change_pump(self, pump_num):
+    def change_pump(self, pump_num): # writing to arduino
         """
             0 for right, 1 for left
         """
@@ -51,13 +51,13 @@ class Ball_Pump:
             # pump_num = 1
             # print(f"pumping right pump {pump_num}")
 
-    def start_left_pump(self):
+    def start_left_pump(self): # simple command for left
         self.change_pump(1)
 
-    def start_right_pump(self):
+    def start_right_pump(self): # simple command for right
         self.change_pump(0)
 
-    def stop_pumps(self):
+    def stop_pumps(self): : # stops both pumps from pumping
         self.board.digital[4].write(0)
         self.board.digital[3].write(0)
 
@@ -72,10 +72,10 @@ class Ball_Pump:
         self.stop_pumps()
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': # testing for pumps, if not working as planned. run this file, and literally type anything for input, and pumps should pump alternating as you put new input.
     bp = Ball_Pump()
     while True:
         e = input("Enter which pump: ")
         bp.pump()
 
-#if no work, connect to serial id with arudinmo app opem
+#if the arduinos still refuse to work, open the arduino IDE and upload/verify. also make sure that the right port is being connected to. open the serial monitor to see the print statements  and run python code. the print statements should work and change with what the python is telling arduno to do.
